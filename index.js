285cbe4b7b150fdd8fe6b0459e99bdf60f34eea1
Erik Greyan <erikgreyan99@gmail.com>
2024-08-02 18:44:37 +0400

    Discord Bot

---
+++ index.js
@@ -0,0 +1,26 @@
+import { Client, GatewayIntentBits } from "discord.js";
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const token = process.env.token;
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", message => {
+    if (message.author.bot) return;
+
+    if (message.content === "!hello") {
+        message.channel.send("Hello, world!");
+    }
+
+    if (message.content.toLowerCase() === "ping") {
+        message.reply("Pong!");
+    }
+
+    if (message.content.includes("Discord")) {
+        message.channel.send("Did someone mention Discord? 😊");
+    }
+});
+
+client.login(token);
